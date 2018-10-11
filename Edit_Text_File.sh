703696e4d1bb430047fccb7eed0d7c2fee497257
Dale Brooksby <dale.brooksby@gmail.com>
2018-10-11 12:04:51 +0100

    Added file editing ability

---
+++ Edit_Text_File.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to select a text file and edit it
+
+## Code here to navigate to correct repo
+
+cd TestRepo1
+
+# Get list of files in directory 
+# for user to choose from
+
+printf "The below files are available to edit: \n\n"
+ls -w1 
+printf "\n\n"
+
+# Get file to be edited
+# from user
+read -p "Enter the name of the file to edit: " fileToEdit
+
+nano $fileToEdit
+
+echo "$fileToEdit successfully edited.""
