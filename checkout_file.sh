9d50684163aef467a685399666ef9d6351df11f4
Dale Brooksby <dale.brooksby@gmail.com>
2018-10-11 16:43:16 +0100

    changed checkout file

--- checkout_file.sh
+++ checkout_file.sh
@@ -5,21 +5,22 @@
 #Get the suer to select file that they want to checkout
 
 
+checkoutFile() {
+	clear
 
-clear
+	cd TestRepo1
 
-cd TestRepo1
+	ls -w1 
+	printf "\n\n"
 
-ls -w1 
-printf "\n\n"
+	# Get file to be edited
+	# from user
+	read -p "Enter the name of a file to checkout: " toCheckout
 
-# Get file to be edited
-# from user
-read -p "Enter the name of a file to checkout: " toCheckout
-
-while [ "$toCheckout" == 'checked_out' ] || [ "$toCheckout" == 'repository.log' ] || [ "$toCheckout" == 'backup' ]; do
-	read -p "Invalid file name (you cannot checkout this file): " toCheckout
-done
+	while [ "$toCheckout" == 'checked_out' ] || [ "$toCheckout" == 'repository.log' ] || [ "$toCheckout" == 'backup' ]; do
+		read -p "Invalid file name (you cannot checkout this file): " toCheckout
+	done
 
 
-mv "$toCheckout" "checked_out/"$toCheckout
+	mv "$toCheckout" "checked_out/"$toCheckout
+}
