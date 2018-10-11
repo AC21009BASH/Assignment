cc1338afede442b29989f5046b07bba277437944
Dale Brooksby <dale.brooksby@gmail.com>
2018-10-11 18:03:59 +0100

    Removed need to specifiy repository

--- add_file.sh
+++ add_file.sh
@@ -4,12 +4,12 @@
 add_file()
 {
 	clear
-	local userInputRepository
 	local userInputFile
 	
 	##asks and saves the repository and file name, adds the file to the chosen repository 
-	read -p "Enter the name of the repository you'd like to add the file to: " userInputRepository
-	read -p "Enter the name of the file (include .txt): " userInputFile
+	read -p "Enter the name of the file: " userInputFile
 
-	echo > ./$userInputRepository/$userInputFile
+	touch $userInputFile
+
+	printf "\n $userInputFile was added to directory ${PWD##*/} \n" >> repository.log
 }
