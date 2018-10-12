608b0dbb7a47f50e56482f63ec6847bddc16dda4
Dale Brooksby <dale.brooksby@gmail.com>
2018-10-12 16:13:53 +0100

    Comments

--- checkoutFile.sh
+++ checkoutFile.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
+
+# Variables to store Date and Time
 DATE=$(date +"%Y-%m-%d")
 TIME=$(date +"%H:%M:%S")
 
+# Clear the screen
 clear
 
+# List all files except script required files and directories
 ls -w1 -I "repository.log" -I "checked_out" -I "backups" -I "repositories.log"
 printf "\n \n"
 
-read -p "Enter the name of a file to checkout: " toCheckout
-
 finished=false;
 while [ $finished == false ]; do
-	read -p "Enter the name of a file to add: " toCheckout
-
+	# Ask the user to enter the name of a file to checkout
+	read -p "Enter the name of a file to checkout: " toCheckout
+	# Check the user is not trying to chekcout script necessary files
 	if [ "$toCheckout" == 'checked_out' ] || [ "$toCheckout" == 'repository.log' ] || [ "$toCheckout" == 'backup' ]
 	then
+		# Send error message and add entry to log file
 		echo "Invalid file name (you cannot checkout this file)"
 		printf "$USER $UID attempted to checkout the file $toCheckout\t\t\t $DATE at $TIME\n" >> repository.log
+	# If the file exists
 	elif [ -f ./"$toCheckout" ]
 	then
 		finished=true;
+	# If the file does not exist
 	else
-		echo "Invalid file name (file does already exists)"
-		printf "$USER $UID attempted do add existing file $toCheckout in the repository ${PWD##*/}\t\t $DATE at $TIME\n" >> repository.log
+		# Send error message and add entry to log file
+		echo "Invalid file name (file does not exist)"
+		printf "$USER $UID checkut nonexistant $toCheckout in the repository ${PWD##*/}\t\t $DATE at $TIME\n" >> repository.log
 	fi
 done
 
+# move the file to the checked_out folder where edits are allowed
 mv "$toCheckout" "checked_out/"$toCheckout
 
+# Add entry to log file
 printf "$USER $UID checked out the file $toCheckout\t\t\t $DATE at $TIME\n" >> repository.log 
