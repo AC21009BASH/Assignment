d4740d4231c06b01fdbb4fa42128f72c50f33b98
Dale Brooksby <dale.brooksby@gmail.com>
2018-10-12 15:52:36 +0100

    Changed validation

--- checkoutFile.sh
+++ checkoutFile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DATE=$(date +"%Y-%m-%d")
+TIME=$(date +"%H:%M:%S")
 
 clear
 
@@ -7,15 +9,21 @@ printf "\n \n"
 
 read -p "Enter the name of a file to checkout: " toCheckout
 
-# Check the user isn't trying to break the script by deleting
-# required files and folders.
-while [ "$toCheckout" == 'checked_out' ] || [ "$toCheckout" == 'repository.log' ] || [ "$toCheckout" == 'backup' ]; do
-	read -p "Invalid file name (you cannot checkout this file): " toCheckout
+finished=false;
+while [ $finished == false ]; do
+	read -p "Enter the name of a file to add: " toCheckout
 
-	DATE=$(date +"%Y-%m-%d")
-	TIME=$(date +"%H:%M:%S")
-
-    printf "$USER $UID attempted to checkout the file $toCheckout\t\t\t $DATE at $TIME\n" >> repository.log 
+	if [ "$toCheckout" == 'checked_out' ] || [ "$toCheckout" == 'repository.log' ] || [ "$toCheckout" == 'backup' ]
+	then
+		echo "Invalid file name (you cannot checkout this file)"
+		printf "$USER $UID attempted to checkout the file $toCheckout\t\t\t $DATE at $TIME\n" >> repository.log
+	elif [ -f ./"$toCheckout" ]
+	then
+		finished=true;
+	else
+		echo "Invalid file name (file does already exists)"
+		printf "$USER $UID attempted do add existing file $toCheckout in the repository ${PWD##*/}\t\t $DATE at $TIME\n" >> repository.log
+	fi
 done
 
 mv "$toCheckout" "checked_out/"$toCheckout
